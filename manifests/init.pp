47e2bd1816825b59aa49a8674443f3ae0cd6a911
Jordi Prats <jprats@systemadmin.es>
2016-03-31 11:26:16 +0200

    lint tabs a espais

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 #
 class sssd () inherits sssd::params {
 
-	Exec {
-		path => '/bin:/sbin:/usr/bin:/usr/sbin',
-	}
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
 
-	package { $sssd::params::packages:
-		ensure => 'installed',
-	}
+  package { $sssd::params::packages:
+    ensure => 'installed',
+  }
 
-	service { 'messagebus':
-		enable  => true,
-		ensure  => 'running',
-		require => Package['dbus'],
-	}
+  service { 'messagebus':
+    enable  => true,
+    ensure  => 'running',
+    require => Package['dbus'],
+  }
 
 }
--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -1,148 +1,148 @@
 class sssd::ldap(
-			$ldap_uri,
-			$ldap_chpass_uri=undef,
-			$ldap_search_base,
-			$ldap_group_search_base=undef,
-			$ldap_tls_ca_cert = undef,
-			$ldap_schema = 'rfc2307bis',
-			$ldap_tls_reqcert = 'demand',
-			$ldap_group_member = 'member',
-			#$ldap_access_filter = memberOf=cn=...
-			#$ldap_access_filter = (|(memberOf=cn=...)(memberOf=cn=...)...)
-			$ldap_access_filter=undef,
-			$ldap_bind_dn=undef,
-			$ldap_bind_dn_password=undef,
-			$authconfigbackup='/var/tmp/puppet.authconfig.ldap.backup',
-			$filter_users = [ 'root','ldap','named','avahi','haldaemon','dbus','news','nscd' ],
-			$sshkeys=true,
-			$sudoldap=true,
-			$sudoers_order = [ 'files', 'sss' ],
-			$ssl='yes',
-		) inherits sssd::params
+      $ldap_uri,
+      $ldap_chpass_uri=undef,
+      $ldap_search_base,
+      $ldap_group_search_base=undef,
+      $ldap_tls_ca_cert = undef,
+      $ldap_schema = 'rfc2307bis',
+      $ldap_tls_reqcert = 'demand',
+      $ldap_group_member = 'member',
+      #$ldap_access_filter = memberOf=cn=...
+      #$ldap_access_filter = (|(memberOf=cn=...)(memberOf=cn=...)...)
+      $ldap_access_filter=undef,
+      $ldap_bind_dn=undef,
+      $ldap_bind_dn_password=undef,
+      $authconfigbackup='/var/tmp/puppet.authconfig.ldap.backup',
+      $filter_users = [ 'root','ldap','named','avahi','haldaemon','dbus','news','nscd' ],
+      $sshkeys=true,
+      $sudoldap=true,
+      $sudoers_order = [ 'files', 'sss' ],
+      $ssl='yes',
+    ) inherits sssd::params
 {
-	Exec {
-		path => '/bin:/sbin:/usr/bin:/usr/sbin',
-	}
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
 
-	validate_array($ldap_uri)
+  validate_array($ldap_uri)
 
-	if($ldap_chpass_uri)
-	{
-		validate_array($ldap_chpass_uri)
-	}
+  if($ldap_chpass_uri)
+  {
+    validate_array($ldap_chpass_uri)
+  }
 
-	if($ldap_tls_ca_cert==undef) and ($ldap_tls_reqcert=='demand')
-	{
-		fail('Incompatible options: ldap_tls_ca_cert undefined, ldap_tls_reqcert demand')
-	}
+  if($ldap_tls_ca_cert==undef) and ($ldap_tls_reqcert=='demand')
+  {
+    fail('Incompatible options: ldap_tls_ca_cert undefined, ldap_tls_reqcert demand')
+  }
 
-	validate_string($ldap_search_base)
+  validate_string($ldap_search_base)
 
-	validate_absolute_path($authconfigbackup)
+  validate_absolute_path($authconfigbackup)
 
-	if($sudoldap)
-	{
-		$nsswitch_opts_sudoers=$sudoers_order
-	}
-	else
-	{
-		$nsswitch_opts_sudoers = [ 'files' ]
-	}
+  if($sudoldap)
+  {
+    $nsswitch_opts_sudoers=$sudoers_order
+  }
+  else
+  {
+    $nsswitch_opts_sudoers = [ 'files' ]
+  }
 
-	service { 'oddjobd':
-		enable  => true,
-		ensure  => 'running',
-		require => Service['messagebus'],
-	}
+  service { 'oddjobd':
+    enable  => true,
+    ensure  => 'running',
+    require => Service['messagebus'],
+  }
 
-	exec { 'authconfig backup':
-		command => "authconfig --savebackup=$authconfigbackup",
-		creates => $authconfigbackup,
-		require => [ Package[$sssd::packages] ],
-	}
+  exec { 'authconfig backup':
+    command => "authconfig --savebackup=$authconfigbackup",
+    creates => $authconfigbackup,
+    require => [ Package[$sssd::packages] ],
+  }
 
-	file { "/etc/sssd/sssd.conf":
-		ensure  => present,
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0600',
-		require => Exec['authconfig backup'],
-		notify  => [ Class['sssd::service'], Exec['authconfig enablesssd'] ],
-		content => template("${module_name}/sssdconf.erb"),
-	}
+  file { "/etc/sssd/sssd.conf":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    require => Exec['authconfig backup'],
+    notify  => [ Class['sssd::service'], Exec['authconfig enablesssd'] ],
+    content => template("${module_name}/sssdconf.erb"),
+  }
 
-	if($ldap_tls_ca_cert!=undef)
-	{
-		exec { 'mkdir openldapcerts':
-			command => "mkdir -p /etc/openldap/cacerts",
-			require => Exec['authconfig backup'],
-		}
+  if($ldap_tls_ca_cert!=undef)
+  {
+    exec { 'mkdir openldapcerts':
+      command => "mkdir -p /etc/openldap/cacerts",
+      require => Exec['authconfig backup'],
+    }
 
-		file { '/etc/openldap/cacerts':
-			ensure  => directory,
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0755',
-			require => Exec['mkdir openldapcerts'],
-		}
+    file { '/etc/openldap/cacerts':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      require => Exec['mkdir openldapcerts'],
+    }
 
-		file { '/etc/openldap/cacerts/sssd.ca':
-			ensure  => present,
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			require => File['/etc/openldap/cacerts'],
-			notify  => Exec['cacertdir rehash'],
-			source  => $ldap_tls_ca_cert,
-		}
+    file { '/etc/openldap/cacerts/sssd.ca':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => File['/etc/openldap/cacerts'],
+      notify  => Exec['cacertdir rehash'],
+      source  => $ldap_tls_ca_cert,
+    }
 
-		exec { 'cacertdir rehash':
-			command      => '/usr/sbin/cacertdir_rehash /etc/openldap/cacerts',
-			refreshonly  => true,
-			require      => File['/etc/openldap/cacerts/sssd.ca'],
-			before 			 => Exec['authconfig enablesssd'],
-			notify       => Exec['authconfig enablesssd'],
-		}
-	}
+    exec { 'cacertdir rehash':
+      command      => '/usr/sbin/cacertdir_rehash /etc/openldap/cacerts',
+      refreshonly  => true,
+      require      => File['/etc/openldap/cacerts/sssd.ca'],
+      before        => Exec['authconfig enablesssd'],
+      notify       => Exec['authconfig enablesssd'],
+    }
+  }
 
-	exec { 'authconfig enablesssd':
-		command     => 'authconfig --enablemkhomedir --enablesssd --enablesssdauth --enablelocauthorize --update',
-		require     => [ Service['oddjobd'], File['/etc/sssd/sssd.conf'] ],
-		refreshonly => true,
-	}
+  exec { 'authconfig enablesssd':
+    command     => 'authconfig --enablemkhomedir --enablesssd --enablesssdauth --enablelocauthorize --update',
+    require     => [ Service['oddjobd'], File['/etc/sssd/sssd.conf'] ],
+    refreshonly => true,
+  }
 
-	class { 'sssd::service':
-		enable  => true,
-		ensure  => 'running',
-		require => Exec['authconfig enablesssd'],
-	}
+  class { 'sssd::service':
+    enable  => true,
+    ensure  => 'running',
+    require => Exec['authconfig enablesssd'],
+  }
 
-	#passwd shadow group
+  #passwd shadow group
 
-	class { 'nsswitch':
-		passwd   => [ 'files', 'sss' ],
-		shadow   => [ 'files', 'sss' ],
-		group    => [ 'files', 'sss' ],
-		#gshadow => [ 'files', 'sss' ],
-		sudoers  => $nsswitch_opts_sudoers,
-		notify   => Class['sssd::service'],
-	}
+  class { 'nsswitch':
+    passwd   => [ 'files', 'sss' ],
+    shadow   => [ 'files', 'sss' ],
+    group    => [ 'files', 'sss' ],
+    #gshadow => [ 'files', 'sss' ],
+    sudoers  => $nsswitch_opts_sudoers,
+    notify   => Class['sssd::service'],
+  }
 
-	if($sshkeys)
-	{
-		package { 'openssh-ldap':
-			ensure => 'installed',
-			before => Class['sssd::service'],
-		}
+  if($sshkeys)
+  {
+    package { 'openssh-ldap':
+      ensure => 'installed',
+      before => Class['sssd::service'],
+    }
 
-		file { '/etc/ssh/ldap.conf':
-			owner   => 'root',
-			group   => 'sshd',
-			mode    => '0640',
-			content => template("${module_name}/ldap-sshkeys.erb"),
-			require => Package['openssh-ldap'],
-			notify  => Class['sssd::service'],
-			before  => Class['sssd::service'],
-		}
-	}
+    file { '/etc/ssh/ldap.conf':
+      owner   => 'root',
+      group   => 'sshd',
+      mode    => '0640',
+      content => template("${module_name}/ldap-sshkeys.erb"),
+      require => Package['openssh-ldap'],
+      notify  => Class['sssd::service'],
+      before  => Class['sssd::service'],
+    }
+  }
 }
