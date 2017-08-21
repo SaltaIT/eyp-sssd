9c65af8c8e32d1660d8a21b4232e0f460a8a2705
Jordi Prats <jprats@systemadmin.es>
2017-08-21 08:55:31 +0200

    millores gestió sssd monit

--- manifests/monit.pp
+++ manifests/monit.pp
@@ -1,16 +1,28 @@
 class sssd::monit(
+                    $ensure      = 'present',
+                    $file_owner  = 'root',
+                    $file_group  = 'root',
+                    $file_mode   = '0750',
                     $basedir     = '/usr/local/bin',
                     $script_name = 'check_sssd_user',
-                    $ensure      = 'present',
                   ) inherits sssd::params {
 
   # falta acceptance testing
 
   file { "${basedir}/${script_name}":
     ensure  => $ensure,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0750',
+    owner   => $file_owner,
+    group   => $file_group,
+    mode    => $file_mode,
     content => file("${module_name}/check_sssd_user.sh"),
+  }
+
+  if(defined(Class['sudoers']))
+  {
+    sudoers::sudo { "sudo root ${basedir}/${script_name}":
+      ensure   => $ensure,
+      username => 'root',
+      command  => "${basedir}/${script_name}",
+    }
   }
 }
